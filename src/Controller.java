09614669b3df6986d5b72cc4de75793514b6655d
Mateusz Sukiennik <m.sukiennik@stud.elka.pw.edu.pl>
2020-11-16 00:53:37 +0100

    adding proto controllers for buttons

--- src/Controller.java
+++ src/Controller.java
@@ -1,2 +1,18 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class Controller {
+
+    @FXML
+    private void handleLogin(ActionEvent event) {
+        event.consume();
+        System.out.println("Logging in...");
+    }
+
+    @FXML
+    private void handleRegister(ActionEvent event) {
+        event.consume();
+        System.out.println("Registered.");
+    }
+
 }
