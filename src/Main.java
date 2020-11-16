f2442973ab40c844258ae4ee3270eef8e38e64cc
Mateusz Sukiennik <m.sukiennik@stud.elka.pw.edu.pl>
2020-11-16 17:35:16 +0100

    testing GUI menu

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,36 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.ListView;
+import javafx.event.ActionEvent;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 
 public class Main extends Application {
 
+    static Stage stg;
+
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        this.stg = primaryStage;
+
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.setTitle("UNO");
         primaryStage.setScene(new Scene(root, 1200, 800));
+
         primaryStage.show();
     }
 
